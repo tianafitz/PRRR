63814ce5cfa207ccd6800955f171170b2b059250
Andy Jones <ajones788@gmail.com>
2021-03-22 12:20:50 -0400

    format code with black

--- experiments/simulations/parameter_recovery_experiment.py
+++ experiments/simulations/parameter_recovery_experiment.py
@@ -4,15 +4,17 @@ import seaborn as sns
 import pandas as pd
 from os.path import join as pjoin
 import sys
+
 sys.path.append("../../models")
 from prrr_nb_tfp import fit_rrr
 from rrr_tfp_gaussian import fit_rrr as fit_rrr_gaussian
 from sklearn.model_selection import train_test_split
 
 import matplotlib
-font = {'size': 30}
-matplotlib.rc('font', **font)
-matplotlib.rcParams['text.usetex'] = True
+
+font = {"size": 30}
+matplotlib.rc("font", **font)
+matplotlib.rcParams["text.usetex"] = True
 
 ## Simulate data
 
@@ -27,144 +29,138 @@ q_list = [10, 50, 100]
 
 def test_rrr_gaussian_varyn():
 
-	p = 10
-	q = 10
-	k = 5
-	mses_AB = np.zeros((num_repeats, len(n_list)))
+    p = 10
+    q = 10
+    k = 5
+    mses_AB = np.zeros((num_repeats, len(n_list)))
 
-	for ii in range(num_repeats):
-		for jj, n in enumerate(n_list):
-			A_true = np.random.normal(size=(p, k))
-			B_true = np.random.normal(size=(k, q))
-			AB_true = A_true @ B_true
-			X = np.random.normal(size=(n, p))
-			Y_mean = X @ A_true @ B_true
-			Y = Y_mean + np.random.normal(size=(n, q))
+    for ii in range(num_repeats):
+        for jj, n in enumerate(n_list):
+            A_true = np.random.normal(size=(p, k))
+            B_true = np.random.normal(size=(k, q))
+            AB_true = A_true @ B_true
+            X = np.random.normal(size=(n, p))
+            Y_mean = X @ A_true @ B_true
+            Y = Y_mean + np.random.normal(size=(n, q))
 
-			# Fit model
-			rrr_results = fit_rrr_gaussian(X=X, Y=Y, k=k)
+            # Fit model
+            rrr_results = fit_rrr_gaussian(X=X, Y=Y, k=k)
 
-			# Compute MSE
-			A_est = rrr_results['A_mean'].numpy()
-			B_est = rrr_results['B_mean'].numpy()
-			AB_est = A_est @ B_est
-			mse_AB = np.mean((AB_est - AB_true)**2)
-			print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
+            # Compute MSE
+            A_est = rrr_results["A_mean"].numpy()
+            B_est = rrr_results["B_mean"].numpy()
+            AB_est = A_est @ B_est
+            mse_AB = np.mean((AB_est - AB_true) ** 2)
+            print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
 
-			mses_AB[ii, jj] = mse_AB
+            mses_AB[ii, jj] = mse_AB
 
-	mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
+    mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
 
+    plt.figure(figsize=(10, 8))
+    sns.boxplot(data=mse_AB_df, x="variable", y="value")
+    plt.xlabel(r"$n$")
+    plt.ylabel(r"$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$")
+    plt.title("Gaussian RRR")
+    plt.tight_layout()
+    plt.savefig(pjoin(FIGURE_DIR, "rrr_gaussian_MSE_varyn.png"))
+    plt.show()
 
-	plt.figure(figsize=(10, 8))
-	sns.boxplot(data=mse_AB_df, x="variable", y="value")
-	plt.xlabel(r'$n$')
-	plt.ylabel(r'$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$')
-	plt.title("Gaussian RRR")
-	plt.tight_layout()
-	plt.savefig(pjoin(FIGURE_DIR, "rrr_gaussian_MSE_varyn.png"))
-	plt.show()
 
 def test_rrr_poisson_varyn():
 
-	p = 10
-	q = 10
-	k = 5
-	mses_AB = np.zeros((num_repeats, len(n_list)))
+    p = 10
+    q = 10
+    k = 5
+    mses_AB = np.zeros((num_repeats, len(n_list)))
 
-	for ii in range(num_repeats):
-		for jj, n in enumerate(n_list):
-			A_true = np.random.gamma(1, 1, size=(p, k))
-			B_true = np.random.gamma(1, 1, size=(k, q))
-			AB_true = A_true @ B_true
-			X = np.random.gamma(1, 1, size=(n, p))
-			Y_mean = X @ A_true @ B_true
-			Y = np.random.poisson(Y_mean)
+    for ii in range(num_repeats):
+        for jj, n in enumerate(n_list):
+            A_true = np.random.gamma(1, 1, size=(p, k))
+            B_true = np.random.gamma(1, 1, size=(k, q))
+            AB_true = A_true @ B_true
+            X = np.random.gamma(1, 1, size=(n, p))
+            Y_mean = X @ A_true @ B_true
+            Y = np.random.poisson(Y_mean)
 
-			# Fit model
-			rrr_results = fit_rrr(X=X, Y=Y, k=k)
+            # Fit model
+            rrr_results = fit_rrr(X=X, Y=Y, k=k)
 
-			# Compute MSE
-			A_lognormal_mean = rrr_results['A_mean'].numpy()
-			A_stddev = rrr_results['A_stddev'].numpy()
-			B_lognormal_mean = rrr_results['B_mean'].numpy()
-			B_stddev = rrr_results['B_stddev'].numpy()
+            # Compute MSE
+            A_lognormal_mean = rrr_results["A_mean"].numpy()
+            A_stddev = rrr_results["A_stddev"].numpy()
+            B_lognormal_mean = rrr_results["B_mean"].numpy()
+            B_stddev = rrr_results["B_stddev"].numpy()
 
-			A_est = np.exp(A_lognormal_mean + 0.5 * A_stddev**2)
-			B_est = np.exp(B_lognormal_mean + 0.5 * B_stddev**2)
-			AB_est = A_est @ B_est
-			mse_AB = np.mean((AB_est - AB_true)**2)
-			print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
+            A_est = np.exp(A_lognormal_mean + 0.5 * A_stddev ** 2)
+            B_est = np.exp(B_lognormal_mean + 0.5 * B_stddev ** 2)
+            AB_est = A_est @ B_est
+            mse_AB = np.mean((AB_est - AB_true) ** 2)
+            print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
 
-			mses_AB[ii, jj] = mse_AB
+            mses_AB[ii, jj] = mse_AB
 
+    mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
 
-	mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
+    plt.figure(figsize=(10, 8))
+    sns.boxplot(data=mse_AB_df, x="variable", y="value")
+    plt.xlabel(r"$n$")
+    plt.ylabel(r"$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$")
+    plt.title("PRRR")
+    plt.tight_layout()
+    plt.savefig(pjoin(FIGURE_DIR, "rrr_poisson_MSE_varyn.png"))
+    plt.savefig("../../figures/paper_figures/figure2.pdf", bbox_inches="tight")
+    plt.show()
 
-
-	plt.figure(figsize=(10, 8))
-	sns.boxplot(data=mse_AB_df, x="variable", y="value")
-	plt.xlabel(r'$n$')
-	plt.ylabel(r'$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$')
-	plt.title("PRRR")
-	plt.tight_layout()
-	plt.savefig(pjoin(FIGURE_DIR, "rrr_poisson_MSE_varyn.png"))
-	plt.savefig("../../figures/paper_figures/figure2.pdf", bbox_inches='tight')
-	plt.show()
 
 def test_rrr_poisson_varyq():
 
-	p = 10
-	k = 5
-	n = 100
+    p = 10
+    k = 5
+    n = 100
 
-	mses_AB = np.zeros((num_repeats, len(n_list)))
+    mses_AB = np.zeros((num_repeats, len(n_list)))
 
-	for ii in range(num_repeats):
-		for jj, q in enumerate(q_list):
-			A_true = np.random.gamma(1, 1, size=(p, k))
-			B_true = np.random.gamma(1, 1, size=(k, q))
-			AB_true = A_true @ B_true
-			X = np.random.gamma(1, 1, size=(n, p))
-			Y_mean = X @ A_true @ B_true
-			Y = np.random.poisson(Y_mean)
+    for ii in range(num_repeats):
+        for jj, q in enumerate(q_list):
+            A_true = np.random.gamma(1, 1, size=(p, k))
+            B_true = np.random.gamma(1, 1, size=(k, q))
+            AB_true = A_true @ B_true
+            X = np.random.gamma(1, 1, size=(n, p))
+            Y_mean = X @ A_true @ B_true
+            Y = np.random.poisson(Y_mean)
 
-			# Fit model
-			rrr_results = fit_rrr(X=X, Y=Y, k=k)
+            # Fit model
+            rrr_results = fit_rrr(X=X, Y=Y, k=k)
 
-			# Compute MSE
-			A_lognormal_mean = rrr_results['A_mean'].numpy()
-			A_stddev = rrr_results['A_stddev'].numpy()
-			B_lognormal_mean = rrr_results['B_mean'].numpy()
-			B_stddev = rrr_results['B_stddev'].numpy()
+            # Compute MSE
+            A_lognormal_mean = rrr_results["A_mean"].numpy()
+            A_stddev = rrr_results["A_stddev"].numpy()
+            B_lognormal_mean = rrr_results["B_mean"].numpy()
+            B_stddev = rrr_results["B_stddev"].numpy()
 
-			A_est = np.exp(A_lognormal_mean + 0.5 * A_stddev**2)
-			B_est = np.exp(B_lognormal_mean + 0.5 * B_stddev**2)
-			AB_est = A_est @ B_est
-			mse_AB = np.mean((AB_est - AB_true)**2)
-			print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
+            A_est = np.exp(A_lognormal_mean + 0.5 * A_stddev ** 2)
+            B_est = np.exp(B_lognormal_mean + 0.5 * B_stddev ** 2)
+            AB_est = A_est @ B_est
+            mse_AB = np.mean((AB_est - AB_true) ** 2)
+            print("n: {}, AB MSE: {}".format(n, round(mse_AB, 3)))
 
-			mses_AB[ii, jj] = mse_AB
+            mses_AB[ii, jj] = mse_AB
 
+    mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
 
-	mse_AB_df = pd.melt(pd.DataFrame(mses_AB, columns=n_list))
-
-
-	plt.figure(figsize=(10, 8))
-	sns.boxplot(data=mse_AB_df, x="variable", y="value")
-	plt.xlabel(r'$q$')
-	plt.ylabel(r'$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$')
-	plt.title("PRRR")
-	plt.tight_layout()
-	plt.savefig(pjoin(FIGURE_DIR, "rrr_poisson_MSE_varyq.png"))
-	plt.show()
+    plt.figure(figsize=(10, 8))
+    sns.boxplot(data=mse_AB_df, x="variable", y="value")
+    plt.xlabel(r"$q$")
+    plt.ylabel(r"$\|\widehat{A} \widehat{B}-A^\star B^\star\|_2$")
+    plt.title("PRRR")
+    plt.tight_layout()
+    plt.savefig(pjoin(FIGURE_DIR, "rrr_poisson_MSE_varyq.png"))
+    plt.show()
 
 
 if __name__ == "__main__":
 
-	# test_rrr_gaussian_varyn()
-	test_rrr_poisson_varyn()
-	# test_rrr_poisson_varyq()
-
-
-
+    # test_rrr_gaussian_varyn()
+    test_rrr_poisson_varyn()
+    # test_rrr_poisson_varyq()
--- models/rrr_tfp.py
+++ models/rrr_tfp.py
@@ -18,7 +18,7 @@ from scipy.stats import multivariate_normal
 tf.enable_v2_behavior()
 
 plt.style.use("ggplot")
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 NUM_VI_ITERS = 300
 LEARNING_RATE_VI = 0.05
@@ -26,20 +26,22 @@ LEARNING_RATE_VI = 0.05
 
 # ------- Specify model ---------
 
+
 def rrr(X, q, k):
 
     n, p = X.shape
 
-    A = yield tfd.Gamma(concentration=tf.fill([p, k], 2.0),
-                 rate=tf.ones([p, k]),
-                 name="A")
+    A = yield tfd.Gamma(
+        concentration=tf.fill([p, k], 2.0), rate=tf.ones([p, k]), name="A"
+    )
 
-    B = yield tfd.Gamma(concentration=tf.fill([k, q], 2.0),
-                 rate=tf.ones([k, q]),
-                 name="B")
+    B = yield tfd.Gamma(
+        concentration=tf.fill([k, q], 2.0), rate=tf.ones([k, q]), name="B"
+    )
 
-    Y = yield tfd.Poisson(rate=tf.matmul(tf.matmul(X.astype("float32"), A), B),
-                     name="Y")
+    Y = yield tfd.Poisson(
+        rate=tf.matmul(tf.matmul(X.astype("float32"), A), B), name="Y"
+    )
 
 
 def fit_rrr(X, Y, k):
@@ -53,29 +55,27 @@ def fit_rrr(X, Y, k):
 
     model = tfd.JointDistributionCoroutineAutoBatched(rrr_model)
 
-    def target_log_prob_fn(A, B): return model.log_prob((A, B, Y))
+    def target_log_prob_fn(A, B):
+        return model.log_prob((A, B, Y))
 
     # ------- Specify variational families -----------
 
     # Variational parmater means
-    
+
     qA_concentration = tf.fill([p, k], 2.0)
     qA_rate = tfp.util.TransformedVariable(tf.ones([p, k]), bijector=tfb.Softplus())
-    
+
     qB_concentration = tf.fill([k, q], 2.0)
     qB_rate = tfp.util.TransformedVariable(tf.ones([k, q]), bijector=tfb.Softplus())
-    
+
     def factored_normal_variational_model():
-      qA = yield tfd.Gamma(concentration=qA_concentration,
-                           rate=qA_rate,
-                            name="qA")
-      qB = yield tfd.Gamma(concentration=qB_concentration,
-                           rate=qB_rate,
-                            name="qB")
+        qA = yield tfd.Gamma(concentration=qA_concentration, rate=qA_rate, name="qA")
+        qB = yield tfd.Gamma(concentration=qB_concentration, rate=qB_rate, name="qB")
 
     # Surrogate posterior that we will try to make close to p
     surrogate_posterior = tfd.JointDistributionCoroutineAutoBatched(
-        factored_normal_variational_model)
+        factored_normal_variational_model
+    )
 
     # --------- Fit variational inference model using MC samples and gradient descent ----------
 
@@ -83,10 +83,11 @@ def fit_rrr(X, Y, k):
         target_log_prob_fn,
         surrogate_posterior=surrogate_posterior,
         optimizer=tf.optimizers.Adam(learning_rate=LEARNING_RATE_VI),
-        num_steps=NUM_VI_ITERS)
+        num_steps=NUM_VI_ITERS,
+    )
 
     return_dict = {
-    'loss_trace': losses,
+        "loss_trace": losses,
     }
 
     # plt.scatter(A_true[:, 0], qA_mean.numpy()[:, 0])
@@ -100,12 +101,11 @@ def naive_bayes(X, q):
 
     n, p = X.shape
 
-    lam = yield tfd.Gamma(concentration=tf.fill([p, q], 2.0),
-                 rate=tf.ones([p, q]),
-                 name="A")
+    lam = yield tfd.Gamma(
+        concentration=tf.fill([p, q], 2.0), rate=tf.ones([p, q]), name="A"
+    )
 
-    Y = yield tfd.Poisson(rate=tf.matmul(X.astype("float32"), lam),
-                        name="Y")
+    Y = yield tfd.Poisson(rate=tf.matmul(X.astype("float32"), lam), name="Y")
 
 
 def fit_naive_bayes(X, Y):
@@ -120,24 +120,25 @@ def fit_naive_bayes(X, Y):
 
     model = tfd.JointDistributionCoroutineAutoBatched(nb_model)
 
-    def target_log_prob_fn(lam): return model.log_prob(
-        (lam, Y))
+    def target_log_prob_fn(lam):
+        return model.log_prob((lam, Y))
 
     # ------- Specify variational families -----------
 
     # Variational parmater means
-    
+
     qlam_concentration = tf.fill([p, q], 2.0)
     qlam_rate = tfp.util.TransformedVariable(tf.ones([p, q]), bijector=tfb.Softplus())
 
     def factored_normal_variational_model():
-      qlam = yield tfd.Gamma(concentration=qlam_concentration,
-                 rate=qlam_rate,
-                 name="qlam")
+        qlam = yield tfd.Gamma(
+            concentration=qlam_concentration, rate=qlam_rate, name="qlam"
+        )
 
     # Surrogate posterior that we will try to make close to p
     surrogate_posterior = tfd.JointDistributionCoroutineAutoBatched(
-        factored_normal_variational_model)
+        factored_normal_variational_model
+    )
 
     # --------- Fit variational inference model using MC samples and gradient descent ----------
 
@@ -145,10 +146,11 @@ def fit_naive_bayes(X, Y):
         target_log_prob_fn,
         surrogate_posterior=surrogate_posterior,
         optimizer=tf.optimizers.Adam(learning_rate=LEARNING_RATE_VI),
-        num_steps=NUM_VI_ITERS)
+        num_steps=NUM_VI_ITERS,
+    )
 
     return_dict = {
-    'loss_trace': losses,
+        "loss_trace": losses,
     }
 
     # plt.scatter(A_true[:, 0], qA_mean.numpy()[:, 0])
@@ -160,49 +162,46 @@ def fit_naive_bayes(X, Y):
 
 if __name__ == "__main__":
 
-  n = 200
-  p = 4
-  q = 20
-  k_true = 5
-  cell_types_ints = np.random.choice(np.arange(p), size=n, replace=True)
-  X = np.zeros((n, p))
-  for ii in range(n):
-    X[ii, cell_types_ints[ii]] = 1
-  A_true = np.random.gamma(2, 1, size=(p, k_true))
-  B_true = np.random.gamma(2, 1, size=(k_true, q))
-  Y_mean = X @ A_true @ B_true
-  Y = np.random.poisson(Y_mean)
+    n = 200
+    p = 4
+    q = 20
+    k_true = 5
+    cell_types_ints = np.random.choice(np.arange(p), size=n, replace=True)
+    X = np.zeros((n, p))
+    for ii in range(n):
+        X[ii, cell_types_ints[ii]] = 1
+    A_true = np.random.gamma(2, 1, size=(p, k_true))
+    B_true = np.random.gamma(2, 1, size=(k_true, q))
+    Y_mean = X @ A_true @ B_true
+    Y = np.random.poisson(Y_mean)
 
-  all_rrr = []
-  all_nb = []
-  k_range = [1, 3, 4, 5, 6, 10, 20]
-  for k_rrr in k_range:
-    rrr_elbo_list = []
-    nb_elbo_list = []
+    all_rrr = []
+    all_nb = []
+    k_range = [1, 3, 4, 5, 6, 10, 20]
+    for k_rrr in k_range:
+        rrr_elbo_list = []
+        nb_elbo_list = []
 
-    for i in range(3):
-        rrr_results = fit_rrr(X=X, Y=Y, k=k_rrr)
-        rrr_elbo = -rrr_results['loss_trace'].numpy()[-1]
-        print("RRR ELBO: {}".format(rrr_elbo))
-        rrr_elbo_list.append(rrr_elbo)
-    
-    
-        nb_results = fit_naive_bayes(X=X, Y=Y)
-        nb_elbo = -nb_results['loss_trace'].numpy()[-1]
-        print("Naive Bayes ELBO: {}".format(nb_elbo))
-        nb_elbo_list.append(nb_elbo)
-    all_rrr.append(rrr_elbo_list)
-    all_nb.append(nb_elbo_list)
+        for i in range(3):
+            rrr_results = fit_rrr(X=X, Y=Y, k=k_rrr)
+            rrr_elbo = -rrr_results["loss_trace"].numpy()[-1]
+            print("RRR ELBO: {}".format(rrr_elbo))
+            rrr_elbo_list.append(rrr_elbo)
 
-  plt.plot(k_range, np.mean(all_rrr, axis=1), 'o-', label="RRR")
-  plt.plot(k_range, np.mean(all_nb, axis=1), 'o-', label="Naive Bayes")
-  plt.axvline(k_true, linestyle='--', label="True rank")
-  plt.legend()
-  plt.xlabel("Rank for RRR")
-  plt.ylabel("ELBO (higher is better)")
-  plt.tight_layout()
-  plt.show()
+            nb_results = fit_naive_bayes(X=X, Y=Y)
+            nb_elbo = -nb_results["loss_trace"].numpy()[-1]
+            print("Naive Bayes ELBO: {}".format(nb_elbo))
+            nb_elbo_list.append(nb_elbo)
+        all_rrr.append(rrr_elbo_list)
+        all_nb.append(nb_elbo_list)
 
-  #import ipdb; ipdb.set_trace()
+    plt.plot(k_range, np.mean(all_rrr, axis=1), "o-", label="RRR")
+    plt.plot(k_range, np.mean(all_nb, axis=1), "o-", label="Naive Bayes")
+    plt.axvline(k_true, linestyle="--", label="True rank")
+    plt.legend()
+    plt.xlabel("Rank for RRR")
+    plt.ylabel("ELBO (higher is better)")
+    plt.tight_layout()
+    plt.show()
 
-
+    # import ipdb; ipdb.set_trace()
